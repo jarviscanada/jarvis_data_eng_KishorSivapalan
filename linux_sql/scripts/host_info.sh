704728e4bb52d0d7e7c1bec51eaa989542281b00
Kishor Sivapalan <kishorsiv11@gmail.com>
2022-07-19 18:11:28 +0000

    functioning hardware info grabber

---
+++ linux_sql/scripts/host_info.sh
@@ -0,0 +1,41 @@
+#! /bin/sh
+
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
+
+if [ $# -ne  5 ]; then
+  echo 'To use: ./scripts/host_info.sh psql_host psql_port db_name psql_user psql_password'
+  exit 1
+fi
+
+#Save machine statistics in MB and current machine hostname to variables
+vmstat_mb=$(vmstat --unit M)
+hostname=$(hostname -f)
+lscpu_out=`lscpu`
+mem_out=`cat /proc/meminfo`
+
+#host hardware information
+cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+cpu_architecture=$(echo "$lscpu_out"  | egrep "^Architecture\:" | awk '{print $2}' | xargs)
+cpu_model=$(echo "$lscpu_out"  | egrep "^Model name\:" | awk '{print $3,$4,$5,$6,$7}' | xargs)
+cpu_mhz=$(echo "$lscpu_out"  | egrep "^CPU MHz\:" | awk '{print $3}' | xargs)
+L2_cache=$(echo "$lscpu_out"  | egrep "^L2 cache:" | awk '{print $3}' | xargs | cut -d'K' -f 1)
+total_mem=$(echo "$mem_out"  | egrep "^MemTotal\:" | awk '{print $2}' | xargs)
+
+#Current time in `2019-11-26 14:40:19` UTC format
+#timestamp=$(vmstat -t | awk ...
+timestamp=$(date '+%Y-%m-%d %T')
+
+#PSQL command: Inserts server usage data into host_usage table
+insert_stmt="INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, L2_cache, total_mem, \"timestamp\")
+VALUES ('$hostname', $cpu_number, '$cpu_architecture', '$cpu_model', $cpu_mhz, $L2_cache, $total_mem, '$timestamp');"
+
+#set up env var for pql cmd
+export PGPASSWORD=$psql_password
+
+#Insert date into a database
+psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
+exit $?
