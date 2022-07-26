067c580a582730559a82f5196d6de1988d730160
Kishor Sivapalan <kishorsiv11@gmail.com>
2022-07-26 05:47:21 +0000

    sql queries completed

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -1,4 +1,4 @@
--- Business Case 1: Group hosts by hardware info
+------- Business Case 1: Group hosts by hardware info -------
 SELECT
     cpu_number,
     id AS host_id,
@@ -10,7 +10,7 @@ GROUP BY
 ORDER BY
     total_mem DESC;
 
--- Business Case 2: Average memory usage
+------- Business Case 2: Average memory usage -------
 CREATE FUNCTION round5(ts timestamp) RETURNS timestamp AS
 $$
 BEGIN
@@ -19,4 +19,31 @@ END;
 $$
     LANGUAGE PLPGSQL;
 
--- Business Case 3: Detect host failure
+SELECT
+    host_id,
+    hostname,
+    round5(host_usage.timestamp) as rounded_time,
+    round((((AVG((total_mem/1000) - memory_free))/(total_mem/1000))*100), 2) as avg_mem_usage
+FROM host_usage
+    JOIN host_info on host_usage.host_id = host_info.id
+GROUP BY
+    host_id,
+    hostname,
+    round_time,
+    total_mem,
+    memory_free
+ORDER BY
+    round_time ASC;
+
+------- Business Case 3: Detect host failure -------
+SELECT
+    host_id,
+    round5(host_usage.timestamp) AS rounded_time,
+    COUNT(*) as num_data_points
+FROM
+    host_usage
+GROUP BY
+    rounded_time,
+    host_id
+HAVING
+    COUNT(*) < 3;
